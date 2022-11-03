3abcc058f50e463fa879f0bb2ab476c95ae445e8
Laísa Rio <laisa.rioverde@gmail.com>
2022-11-03 16:40:03 -0300

    Iniciando o projeto, rotas feitas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Login from '../pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (<Login />);
   }
 }
 
