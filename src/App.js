427459a1a1a0e603c4aabeea3a009a9cade5a112
Laísa Rio <laisa.rioverde@gmail.com>
2022-11-03 17:50:06 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import Login from '../pages/Login';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<Login />);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+<BrowserRouter>
+  <App />
+</BrowserRouter>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<BrowserRouter>
-  <App />
-</BrowserRouter>
