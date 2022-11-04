9a1d93508c465d7f07b7107e8d545a811fe173e1
Laísa Rio <laisa.rioverde@gmail.com>
2022-11-04 20:22:56 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artist: '',
+    isButtonValid: false,
+  };
+
+  handleInputChange = ({ target }) => {
+    const minLegth = 2;
+    if (target.value.length >= minLegth) {
+      this.setState({ isButtonValid: true });
+    }
+    this.setState({ artist: target.value });
+  };
+
   render() {
+    const { artist, isButtonValid } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome do Artista"
+            value={ artist }
+            onChange={ this.handleInputChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ !isButtonValid }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
